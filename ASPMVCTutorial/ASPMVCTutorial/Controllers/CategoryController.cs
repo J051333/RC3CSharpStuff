e611a66dda05780cd9a206aeff97e3527cef0bdb
Josie Scarlett Whitlock <62673737+J051333@users.noreply.github.com>
2022-04-10 21:02:07 -0500

    Chapters through Category Delete completed

--- ASPMVCTutorial/ASPMVCTutorial/Controllers/CategoryController.cs
+++ ASPMVCTutorial/ASPMVCTutorial/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace ASPMVCTutorial.Controllers {
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
-        
+
         // GET
         public IActionResult Create() {
             return View();
@@ -30,9 +30,64 @@ namespace ASPMVCTutorial.Controllers {
             if (ModelState.IsValid) {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-                return Redirect("Index");
+                return RedirectToAction("Index");
             }
             return View(obj);
         }
+
+        // GET
+        public IActionResult Edit(int? id) {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj) {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "The Display Order must not be the same as the Name.");
+            }
+            if (ModelState.IsValid) {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Delete(int? id) {
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id) {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
+
 }
