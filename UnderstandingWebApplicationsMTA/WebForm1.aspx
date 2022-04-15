7e8e367195eb68c7327482a5c7b2a35f5f14a0e1
Josie Scarlett Whitlock <62673737+J051333@users.noreply.github.com>
2022-04-14 22:30:45 -0500

    WebForm1.aspx created and written

--- UnderstandingWebApplicationsMTA/WebForm1.aspx
+++ UnderstandingWebApplicationsMTA/WebForm1.aspx
@@ -4,13 +4,21 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>WebForm1</title>
 </head>
 <body>
+    <% = DateTime.Now.ToShortDateString() %>
+
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="Label1" runat="server"
+                Text="Please enter your name: " /> 
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" Text="Submit" runat="server" OnClick="Button1_Click" />
         </div>
     </form>
-    <h1><% DateTime.Now.ToShortDateString(); %></h1>
 </body>
 </html>
--- UnderstandingWebApplicationsMTA/WebForm1.aspx.cs
+++ UnderstandingWebApplicationsMTA/WebForm1.aspx.cs
@@ -9,6 +9,10 @@ namespace UnderstandingWebApplicationsMTA {
     public partial class WebForm1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             Response.Write("Message from Page_Load. <br />");
+            
+            if (Session["Name"] != null) {
+                Response.Redirect("WebForm2.aspx");
+            }
         }
 
         protected void Page_Init(object sender, EventArgs e) {
@@ -21,6 +25,11 @@ namespace UnderstandingWebApplicationsMTA {
 
         protected void Page_PreInit(object sender, EventArgs e) {
             Response.Write("Message from Page_PreInit. <br />");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            Session.Add("Name", TextBox1.Text);
+            Response.Redirect("WebForm2.aspx");
         }
     }
 }
