9431f0653a2d730e89a051ca011af05076f4fa09
Josie Scarlett Whitlock <62673737+J051333@users.noreply.github.com>
2022-04-14 22:16:42 -0500

    Working on MTA asp stuff

---
+++ UnderstandingWebApplicationsMTA/examples.js
@@ -0,0 +1,17 @@
+﻿function TakeName() {
+    let username = prompt("Please enter your name!");
+
+    let message = "Hello, " + username + ", your name has ";
+
+    if (username.length > 5) {
+        message += "more than ";
+    } else if (username.length < 5) {
+        message += "less than ";
+    } else {
+        message += "exactly ";
+    }
+
+    message += "5 characters.";
+
+    alert(message);
+}
